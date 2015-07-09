e1394f57c53f03c4c7b60ae22817afba6228e63a
aylwinlo <aylwin@aylwinlo.ca>
2015-07-09 00:47:00 -0400

    starting to do nextbus

--- src/app.js
+++ src/app.js
@@ -9,6 +9,35 @@ var UI = require('ui');
 var ajax = require('ajax');
 var Vector2 = require('vector2');
 
+//construct URL
+var commandName = 'predictions';
+var agencyTag = 'ttc';
+var stopId = '4405';
+var additionalParams = '';
+var URL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=' + commandName + '&a=' + agencyTag + '&stopId=' + stopId + '&' + additionalParams;
+
+//misc
+var statusText = 'Downloading data...';
+
+//make the request
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    //success:
+    statusText = 'successfully fetched data';
+    
+    //extract data
+    var 
+  },
+  function(error) {
+    //failure
+    statusText = 'failed fetching data:' + error;
+  }
+);
+
 // Show splash while waiting for data
 var splashWindow = new UI.Window();
 
@@ -16,12 +45,24 @@ var splashWindow = new UI.Window();
 var text = new UI.Text({
   position: new Vector2(0, 0),
   size: new Vector2(144,168),
-  text: 'Downloading weather data...',
+  text: statusText,
   font: 'GOTHIC_28_BOLD',
-  color: 'green',
   textOverflow: 'wrap',
   textAlign: 'center',
-  backgroundColor: 'orange'
+//  color: 'vivid_cerulean',
+//  color: 'vividcerulean',
+//  color: 'VividCerulean',
+//  color: '#00AAFF',
+//  color: 'GColorVividCerulean',
+//  color: 'inchworm', // works
+  color: 'white',
+//   backgroundColor: 'blue_moon'
+//   backgroundColor: 'bluemoon'
+//   backgroundColor: 'BlueMoon'
+//   backgroundColor: '#0055FF'
+//   backgroundColor: 'GColorBlueMoon'
+//   backgroundColor: 'liberty' // works
+  backgroundColor: 'red'
 });
 
 // Add to splashWindow and show
